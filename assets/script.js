38544646653210c7dd4da35a20f1230c94283419
analizajb <96861913+analizajb@users.noreply.github.com>
2022-04-16 00:34:32 -0700

    adding html form for high score

--- assets/script.js
+++ assets/script.js
@@ -1,103 +1,107 @@
 var button = document.getElementById('button')
 button.innerText = 'Start Quiz',
-button.onclick = function(event) {
-     alert('Your quiz starts now!')
- }
+button.onclick = function() {
+     alert('Your quiz starts now!');
+}
 
 var quizBox = document.getElementById('quiz');
 var resultsBox = document.getElementById('results');
 var submitButton = document.getElementById('submit');
-
-// TIMER
-//document.getElementsByClassName('.timer') {
-//    setInterval = 1000
+ //Use client-side storage to store high scores
+ //localStorage.getItem("score");
+ //sessionStorage.getItem("score");
+ 
+ // TIMER
+ //document.getElementsByClassName('.timer') {
+ //    setInterval = 1000
 //}
 var quizQuestions = [
-    {
-        question: "What does DOM stand for?",
-        answers: {
-            1: 'Display On Machine',
-            2: 'Digital Open Mechanism',
-            3: 'Document On Modem',
-            4: 'Document Object Model'
-        },
-        correctAnswer: '4: Document Object Model'
-    },
-    {
-        question: "Where can JavScript code NOT be placed in an HTML document?",
-        answers: {
-            1: 'In the head section.',
-            2: 'In both the head and body section.',
-            3: 'In the footer section.',
-            4: 'In the body section.'
-        },
-        correctAnswer: '3: In the footer section.'
-    },
-    {
-        question: "Select the statement that is true of arrays in Javascript",
-        answers: {
-            1: 'Arrays are used to organize content within a list element.',
-            2: 'Arrays are elements that are resizable and able to contain different data types.',
-            3: 'Arrays are objects that are resizable and able to contain different data types.',
-            4: 'An array is a list of items with single variables.'
-        },
-        correctAnswer: '3: Arrays are objects that are resizable and able to contain different data types.'
-    }
-];
-
-
+     {
+         question: "What does DOM stand for?",
+         answers: {
+             1: 'Display On Machine',
+             2: 'Digital Open Mechanism',
+             3: 'Document On Modem',
+             4: 'Document Object Model'
+         },
+         correctAnswer: '4: Document Object Model'
+     },
+     {
+         question: "Where can JavScript code NOT be placed in an HTML document?",
+         answers: {
+             1: 'In the head section.',
+             2: 'In both the head and body section.',
+             3: 'In the footer section.',
+             4: 'In the body section.'
+         },
+         correctAnswer: '3: In the footer section.'
+     },
+     {
+         question: "Select the statement that is true of arrays in Javascript",
+         answers: {
+             1: 'Arrays are used to organize content within a list element.',
+             2: 'Arrays are elements that are resizable and able to contain different data types.',
+             3: 'Arrays are objects that are resizable and able to contain different data types.',
+             4: 'An array is a list of items with single variables.'
+         },
+         correctAnswer: '3: Arrays are objects that are resizable and able to contain different data types.'
+     }
+ ];
+ 
+ 
 function generateQuiz(questions, quizBox, resultsBox, submitButton) {
-    function showQuestions(questions, quizBox){
-       var output = [];
-       var answers;
-
-       for(var i=0; i<questions.length; i++){
-           answers = [];
-           for(number in questions [i].answers){
-               answers.push(
-                   '<label>'
-                   + '<input type="radio" name="question'+i+'" value="'+number+'">'
-                   + number + ': '
-                   + questions[i].answers[number]
-                + '</label>'
-               );
-            }
-            
-            output.push(
-             questions[i].question + answers.join('')
-            );
-        }
-
-        quizBox.innerHTML = output.join('');
-    }
-
-    function showResults(questions, quizBox, resultsBox) {
-        var answersContainer = quizBox.querySelectorAll('answers');
-        var userAnswer = '';
-        var numCorrect = 0;
-
+     function showQuestions(questions, quizBox){
+        var output = [];
+        var answers;
+ 
         for(var i=0; i<questions.length; i++){
-            // finds answer
-            userAnswer = (answersContainer[i].querySelector('input[name=question0'+i+']:checked')||{}).value;
-            //if correct answer
-            if(userAnswer===questions[i].correctAnswer){
-                //adds to number of correct answers
-                numCorrect++;
-                //color the answers blue
-                answersContainer[i].style.color = 'lightblue';
-            }
-            else{
-                answersContainer[i].style.color = 'red';
-            }
-        }
+            answers = [];
+            for(number in questions [i].answers){
+                answers.push(
+                    '<label>'
+                    + '<input type="radio" name="question'+i+'" value="'+number+'">'
+                    + number + ': '
+                    + questions[i].answers[number]
+                 + '</label>'
+                );
+             }
+             
+             output.push(
+              questions[i].question + answers.join('')
+             );
+         }
+ 
+         quizBox.innerHTML = output.join('');
+     }
+ 
+     function showResults(questions, quizBox, resultsBox) {
+         var answersContainer = quizBox.querySelectorAll('answers');
+         var userAnswer = '';
+         var numCorrect = 0;
+ 
+         for(var i=0; i<questions.length; i++){
+             // finds answer
+             userAnswer = (answersContainer[i].querySelector('input[name=question0'+i+']:checked')||{}).value;
+             //if correct answer
+             if(userAnswer===questions[i].correctAnswer){
+                 //adds to number of correct answers
+                 numCorrect++;
+                 //color the answers blue
+                 answersContainer[i].style.color = 'lightblue';
+             }
+             else{
+                 answersContainer[i].style.color = 'red';
+             }
+         }
+ 
+         resultsBox.innerHTML = numCorrect + ' out of ' + questions.length;
+     }
+ 
+     //shows questions
+     showQuestions(questions, quizBox);
+ 
+ }
 
-        resultsBox.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-
-    //shows questions
-    showQuestions(questions, quizBox);
-
-}
 
 //Submit button - NOT WORKING
 submitButton.onclick = function () {
@@ -121,7 +125,6 @@ submitButton.onclick = function () {
 
 //var startCountdown = setInterval(countdown, 1000);
 
-generateQuiz(quizQuestions, quizBox, resultsBox, submitButton);
 
 //Create variables to store the quiz questions
 
